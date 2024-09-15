228be370b17b48e02869a35f0cbad11f7662053f
Ankur Kaushal <97340597+Avenster@users.noreply.github.com>
2024-09-15 22:26:56 +0530

    hey2

--- src/new.js
+++ src/new.js
@@ -114,7 +114,7 @@ function verifyToken(req, res, next) {
   });
 }
 
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
